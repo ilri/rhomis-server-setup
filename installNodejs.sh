c0c26a301d93e0e3fcbf1028df5db77681551e0f
Ubuntu <ubuntu@ip-172-26-10-93.eu-west-2.compute.internal>
2021-08-26 13:46:22 +0000

    removing unnecessary files

--- installNodejs.sh
+++ installNodejs.sh
@@ -1,6 +1,7 @@
+#!/bin/sh
 sudo apt update
-sudo apt install nodejs
-sudo apt install npm
+sudo apt install nodejs -y
+sudo apt install npm -y
 
 # Allows us to run the server as a production server
 sudo npm install pm2@latest -g
