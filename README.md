476a3cdbcbeb29f41a53bc85125fa27e4c9aaece
l-gorman <lgorman@turing.ac.uk>
2021-08-26 11:54:53 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Configuration for rhomis server
+
+This is a set of bash scripts which can be used to quickly set up everything which is needed for a RHoMIS server
---
+++ cloneRepositories.sh
@@ -0,0 +1,4 @@
+mkdir ../rhomisApplications
+
+# Cloning the authenticator application
+git clone git@github.com:l-gorman/rhomis-authenticator.git ../rhomisApplications
